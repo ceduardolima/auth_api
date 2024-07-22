498bc83d618093028fbfb722090532afb503e81b
Eduardo <ceduardolima2000@gmail.com>
2024-07-22 15:19:45 -0300

    Validando token

---
+++ src/main/java/com/example/authApi/infra/security/SecurityFilter.java
@@ -0,0 +1,42 @@
+package com.example.authApi.infra.security;
+
+import com.example.authApi.domain.account.Account;
+import com.example.authApi.domain.account.AccountRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getToken(request);
+        if (token != null) {
+            String subject = tokenService.getSubject(token);
+            Account account = accountRepository.getReferenceByEmail(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(account, null, account.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null)
+            return authHeader.replace("Bearer", "");
+        return null;
+    }
+}
--- src/main/java/com/example/authApi/infra/security/TokenService.java
+++ src/main/java/com/example/authApi/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.example.authApi.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.authApi.domain.account.Account;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -33,6 +34,15 @@ public class TokenService {
 
     private Instant expirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm).withIssuer("API authApi").build().verify(tokenJWT).getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
     }
 
 }
